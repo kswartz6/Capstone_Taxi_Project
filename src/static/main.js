64254fbbdfccfc1d548623a1e18fafd6058efb63
casthewiz <cschaufe@kent.edu>
2015-09-17 21:50:37 -0400

    Added Angular, global variable for test record, and http call.

--- src/static/main.js
+++ src/static/main.js
@@ -2,7 +2,26 @@
 
 var map = L.map('map').setView([0, 0], 2);
 
+//Hey, so this is shitty. It'll go away and be replaced with local vars later.
+//For now we'll use this global variable for testing purposes.
+var testRecord = {};
 
+
+var app = angular.module("app", []);
+
+//controller for mapView
+app.controller("mapView", function($scope,$http) {
+
+	//Setting up the test api call here.
+	var testResponse = $http.get("/api/test")
+	testResponse.success(function(data, status, headers, config) {
+        console.log(data);
+		testRecord = data;
+		console.log(testRecord);
+    });
+
+
+});
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
