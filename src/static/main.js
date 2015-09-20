5e431cf922c53d3271fda97e6f07262116937947
Cassidy Schaufele <cschaufe@kent.edu>
2015-09-20 19:39:28 -0400

    More layout dashboard changes

--- src/static/main.js
+++ src/static/main.js
@@ -19,9 +19,11 @@ app.controller("mapView", function($scope,$http) {
 		testRecord = data;
 		console.log(testRecord);
     });
-
-
 });
+app.directive("dateState", function($scope){
+	
+})
+
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
